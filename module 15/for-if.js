d617ca7020dfeca7292621d405231e2d908d165f
Akash Roy <akashroy65123@gmail.com>
2025-07-29 23:52:27 +0600

    update

---
+++ module 15/for-if.js
@@ -0,0 +1,22 @@
+const numbers = [12, 98, 32, 35, 21, 25];
+
+console.log(numbers);
+
+
+// for (const num of numbers){
+//     console.log(num);
+// }
+
+
+
+
+const fruits = ['a, b, c, d, e'];
+
+// for(){
+
+// }
+
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat')
+}
