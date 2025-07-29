add8d1680444bd2e265b6ad54fe81d3bcde452ed
Akash Roy <akashroy65123@gmail.com>
2025-07-29 15:38:03 +0600

    update

---
+++ module 14/multiple.js
@@ -0,0 +1,17 @@
+const salary = 25000;
+const bcs = true;
+const height = 68;
+
+// if(salary > 20000 && height > 66){
+  // conditon dui tai true hole kaj korbe
+//     console.log("su------patro");
+// }else{
+//     console.log('onno patro khuji')
+// }
+
+if (salary > 25000 || height > 72){
+    console.log('eso baba kobul')
+}
+else{
+    console.log('vaag tui makbul')
+}
