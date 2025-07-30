d5eb731f4bfb4662e29235c15de63acae8c2c98a
Akash Roy <akashroy65123@gmail.com>
2025-07-30 09:03:31 +0600

    update

---
+++ module 15/while.js
@@ -0,0 +1,13 @@
+const number = 7;
+if (number < 10) {
+    console.log('choto ekta number paici')
+
+}
+console.log('something new is cooking')
+
+// ------while loop__________________
+let num = 0;
+while(num <5){
+    console.log('looooooooooping', num)
+    num = num + 1;
+}
