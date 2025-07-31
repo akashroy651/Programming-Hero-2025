6f11da0fd90a5c8e932e5071475f7fe8df05a776
Akash Roy <akashroy65123@gmail.com>
2025-07-31 16:42:33 +0600

    update

---
+++ module 15/break.js
@@ -0,0 +1,20 @@
+ // for loop use kore
+// for (let i=0; i <15; i++){
+//     console.log(i)
+//     if(i>=10){
+//         break
+//     }
+// }
+// console.log('life after break up');
+
+// while loop use kore
+
+let n=54;
+while(n > 25){
+    
+    if(n<50){
+        break
+    }
+    console.log(n)
+    n--;
+}
