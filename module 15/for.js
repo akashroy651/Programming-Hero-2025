3c7ccb6f3e16209e01db7d9fc88d95506f57cd59
Akash Roy <akashroy65123@gmail.com>
2025-07-31 11:41:33 +0600

    update

---
+++ module 15/for.js
@@ -0,0 +1,17 @@
+//  ata holo while loop
+let num = 0;
+while(num < 5){
+    console.log(num);
+    num++;
+}
+
+// for loop
+/**
+ * for(first part ; second part ; third part)
+ * 
+ * for (loop variable declaration ; loop condition)
+ */
+
+for( let num = 0; num < 5; num++){
+    console.log(num);
+}
