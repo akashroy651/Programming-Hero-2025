b731c5a6187537b10bc5232d0b29e91396a9254f
Akash Roy <akashroy65123@gmail.com>
2025-07-31 13:00:42 +0600

    update

---
+++ module 15/different.js
@@ -0,0 +1,44 @@
+// 3 diye bag korle vag ses 0 hobe
+// for(let i=0; i<=30; i++){
+//     if(i % 3 ===0){
+//         console.log(i)
+//     }
+// }
+
+// 5 diye vag korle vag ses 0 hobe
+// for(let i=0; i<=30; i++){
+//     if(i % 5 ===0){
+//         console.log(i)
+//     }
+// }
+
+// abr dui ta ak sate o diya jai
+//   ||  ata holo je kono akta  ba duto ak sate
+// for(let i=0; i<=30; i++){
+//     if(i % 3 ===0 || i % 5 ===0 ){
+//         console.log(i)
+//     }
+// }
+
+//   && atar mne duto diye jetei hobe
+// 3 r 5 diye vag kora jai amon number
+//  for(let i = 1; i <= 30; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i)
+//     }
+// }
+
+// give me the sum of numbers from 1 to 20 that are divisible by 3
+
+
+let total = 0;
+for (let i = 1; i<=20; i++){
+    if(i % 3 ==0){
+        console.log(i)
+        total = total + i;
+        console.log('total', total)
+
+    }
+}
+
+console.log('total of the number', total)
