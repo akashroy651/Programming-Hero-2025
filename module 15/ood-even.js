4254a132dc49ea86b2abc9421f4165751d517418
Akash Roy <akashroy65123@gmail.com>
2025-07-31 11:56:48 +0600

    update

---
+++ module 15/ood-even.js
@@ -0,0 +1,15 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+// even number   jhor sonkaa
+for( let i = 0; i<= 10; i=i+2){
+    console.log(i)
+}
+
+//odd number   bijhor sonkaa
+
+for(let i=1; i<=10; i=i+2){
+    console.log(i)
+}
