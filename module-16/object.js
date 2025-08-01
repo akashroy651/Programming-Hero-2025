0698c9cf78aaf74d50ca0f87ae32e78712837955
Akash Roy <akashroy65123@gmail.com>
2025-08-01 14:02:27 +0600

    update

---
+++ module-16/object.js
@@ -0,0 +1,27 @@
+// primitive
+// basic
+const age = 21;
+const school = 'khulna';
+
+
+// non-primitive
+// object
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false,
+}
+
+
+// non-primitive
+const subject = {
+    name: 'math',
+    teacher: 'prodip sir',
+    examDate: '21 feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'final Exam',
+        marks: 100,
+    }
+}
