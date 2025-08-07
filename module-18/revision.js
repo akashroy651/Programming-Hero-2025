981d3e640f839e88fc4424ce3030f21eafb4b0c7
Akash Roy <akashroy65123@gmail.com>
2025-08-07 13:32:52 +0600

    Added a new files

---
+++ module-18/revision.js
@@ -0,0 +1,19 @@
+/**
+ * 1. const --> value not going to change
+ * 2. let --> value might change
+ * 3.var --> should not use  (unless exceptional cases)
+ */
+
+const price = 50;  /// number
+// console.log(price)
+const name = 'akash roy me';  // string
+const isPoor =false  // ata boolean
+
+
+// condition
+
+
+
+// loop
+
+
