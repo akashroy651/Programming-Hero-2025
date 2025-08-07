39327a274849416c801060c7c3f1d227670218cf
Akash Roy <akashroy65123@gmail.com>
2025-08-07 16:12:01 +0600

    Added files

--- module-18/array-looping.js
+++ module-18/array-looping.js
@@ -1,5 +1,23 @@
- 
+ const fruits = ['Apple', 'Banana', 'Anar', 'Daav']
 
- for (let i = 0; i< 10; i++){
-    console.log(i)
+ for (let i = 0; i< fruits.length; i++){
+    // console.log(i)
+    // console.log(fruits[i])
+ } 
+
+
+
+ let k=0;
+ while (k < fruits.length){
+    // console.log(fruits[k]);
+    k++;
+ }
+
+
+
+ const prices = [12, 21, 32, 23,26];
+ let n = 0;
+ while (n< prices.length){
+    console.log(prices[n])
+    n++;
  }
---
+++ module-18/reverse.js
@@ -0,0 +1,21 @@
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers)
+numbers.reverse();
+console.log(numbers)
+
+const reversed = [];
+// for ( let i = 0; i<numbers.length; i++){
+//     console.log(numbers[i]);
+//     reversed.unshift(numbers[i]);
+// }
+
+
+// for (let i = numbers.length-1; i>=0; i--){
+// reversed.push(numbers[i]);
+// }
+
+
+//   
+
+// console.log(reversed)
