338c9c9fa900d4b13fc0fcb33b91c3e7ccd4d517
Akash Roy <akashroy65123@gmail.com>
2025-08-07 10:22:12 +0600

    Added new files and changes

--- module 15/different.js
+++ module 15/different.js
@@ -14,11 +14,11 @@
 
 // abr dui ta ak sate o diya jai
 //   ||  ata holo je kono akta  ba duto ak sate
-// for(let i=0; i<=30; i++){
-//     if(i % 3 ===0 || i % 5 ===0 ){
-//         console.log(i)
-//     }
-// }
+for(let i=0; i<=30; i++){
+    if(i % 3 ===0 || i % 5 ===0 ){
+        console.log(i)
+    }
+}
 
 //   && atar mne duto diye jetei hobe
 // 3 r 5 diye vag kora jai amon number
@@ -31,14 +31,14 @@
 // give me the sum of numbers from 1 to 20 that are divisible by 3
 
 
-let total = 0;
-for (let i = 1; i<=20; i++){
-    if(i % 3 ==0){
-        console.log(i)
-        total = total + i;
-        console.log('total', total)
+// let total = 0;
+// for (let i = 1; i<=20; i++){
+//     if(i % 3 ==0){
+//         console.log(i)
+//         total = total + i;
+//         console.log('total', total)
 
-    }
-}
+//     }
+// }
 
-console.log('total of the number', total)
+// console.log('total of the number', total)
