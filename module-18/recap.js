966f885258fcb49e50865f9e63b7cc6ba5c101fa
Akash Roy <akashroy65123@gmail.com>
2025-08-08 00:05:20 +0600

    update

---
+++ module-18/recap.js
@@ -0,0 +1,15 @@
+
+
+
+const book = ['math', 'physics', 'javascript', 'css']
+for (let i =0; i<book.length; i++){
+    console.log(book[i]);
+}
+
+
+const rev_book = book.reverse()
+console.log(rev_book);
+
+
+const sort_book = book.sort()
+console.log(sort_book);
---
+++ module-19/funcation.js
@@ -0,0 +1,8 @@
+const number = 45;
+
+function fanOffkor(){
+    console.log('bose theke uthe dara')
+    console.log('walk towards the switch')
+    console.log('click the switch to off of the switch')
+}
+fanOffkor()
