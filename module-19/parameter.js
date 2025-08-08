b32eeeb51018dc61d638710bf1c46c3f67a8d008
Akash Roy <akashroy65123@gmail.com>
2025-08-08 12:33:43 +0600

    update

---
+++ module-19/parameter.js
@@ -0,0 +1,23 @@
+function square (number){
+    console.log('valu of the perameter', number)
+    const borgo =number * number;
+    console.log('square of the given number is:', borgo)
+}
+square(5);
+
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+add(2,2)
+add(55, 60)
+
+
+function  addAll(a, b, c, d, e){
+    const total = a + b +c + d +e;
+    console.log(total)
+}
+addAll(10, 10, 10, 10, 10)
+
+
