26c61bda83b22e0afb0ccaa4baa8ef4833224e45
Akash Roy <akashroy65123@gmail.com>
2025-08-08 18:20:09 +0600

    update

---
+++ module-19/argument.js
@@ -0,0 +1,19 @@
+function doubleIt (number){
+    double = number * 2;
+    console.log(number, double)
+}
+
+doubleIt(15)
+console.log('--------------------');
+doubleIt(55);
+
+
+
+function difference (num1, num2){
+    const diff = num1 -num2;
+    console.log(num1, num2, 'difference is', diff)
+}
+
+const fatherAge = 40;
+const myAge  = 20;
+difference(fatherAge, myAge)
