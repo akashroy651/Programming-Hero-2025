1988a82e3d45e92209e56094f80e85383f393961
Akash Roy <akashroy65123@gmail.com>
2025-08-25 17:25:50 +0600

    update

--- module-26-peyoo-mobile-app/home.js
+++ module-26-peyoo-mobile-app/home.js
@@ -2,4 +2,13 @@ document.getElementById('add-money')
 .addEventListener('click', function(e){
     e.preventDefault()
     console.log("add bkbcbxudbvfb")
+    const bank= document.getElementById('bank').value
+    const accountNumber = document.getElementById('account-number').value
+    const amount =parseInt(document.getElementById('add-amount').value)
+    const pin = document.getElementById('add-pin').value
+    // console.log(bank,accountNumber,amount,pin)
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+    // console.log(availableBalance)
+    const totalNewAvailableBalance = amount + availableBalance
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance
 })
