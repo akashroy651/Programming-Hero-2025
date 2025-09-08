5b75b3b345bc699439e2e461aa19b797ad5d96a9
Akash Roy <akashroy65123@gmail.com>
2025-09-08 16:07:26 +0600

    added new files

--- moudle-31/triple-equal.js
+++ moudle-31/triple-equal.js
@@ -1,3 +1,6 @@
+
+//  === first check the type.  if type is different, 6hey are different 
+// if type same then checks the value is same --> true , if value is false ->> false
 console.log(2 === 2);
 console.log(1 === 2);
 console.log(true === 2);
