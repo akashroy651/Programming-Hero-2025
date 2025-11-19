86b45ee6b948a3737098025e0a29518b549b075a
Akash Roy <akashroy65123@gmail.com>
2025-11-19 17:19:10 +0600

    aaded new commit

---
+++ module-60/zap-shift-client/src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import bannerImg1 from "../../../assets/banner/banner1.png";
+import bannerImg2 from "../../../assets/banner/banner2.png";
+import bannerImg3 from "../../../assets/banner/banner3.png";
+import { GoArrowUpRight } from "react-icons/go";
+
+
+const Banner = () => {
+  return (
+    <div>
+     <Carousel 
+    //  autoPlay infiniteLoop
+       >
+        
+        {/* Slide 1 */}
+        <div className="relative">
+          <img src={bannerImg1} />
+
+          {/* Button Group (responsive) */}
+          <div className="absolute bottom-10 md:bottom-7 md:left-1/4 -translate-x-1/2 flex items-center gap-3">
+            
+            <button className="btn bg-primary shadow-xl rounded-ee-4xl rounded-tl-4xl py-3 md:py-5 text-xs md:text-base">
+              Track Your Parcel
+            </button>
+
+            <button className="bg-black text-white p-2 md:p-3 text-lg md:text-2xl rounded-full">
+              <GoArrowUpRight />
+            </button>
+
+            <button className="btn badge-outline rounded-ee-4xl rounded-tl-4xl text-xs md:text-base">
+              Track Your Parcel
+            </button>
+
+          </div>
+        </div>
+
+        {/* Slide 2 */}
+        <div className="relative">
+          <img src={bannerImg2}  />
+
+          <div className="absolute bottom-10 md:bottom-16 left-1/2 -translate-x-1/2 flex items-center gap-3">
+            <button className="btn bg-primary shadow-xl rounded-ee-4xl rounded-tl-4xl py-3 md:py-5 text-xs md:text-base">
+              Track Your Parcel
+            </button>
+
+            <button className="bg-black text-white p-2 md:p-3 text-lg md:text-2xl rounded-full">
+              <GoArrowUpRight />
+            </button>
+
+            <button className="btn badge-outline rounded-ee-4xl rounded-tl-4xl text-xs md:text-base">
+              Track Your Parcel
+            </button>
+          </div>
+        </div>
+
+        {/* Slide 3 */}
+        <div className="relative">
+          <img src={bannerImg3}  />
+
+          <div className="absolute bottom-10 md:bottom-16 left-1/2 -translate-x-1/2 flex items-center gap-3">
+            <button className="btn bg-primary shadow-xl rounded-ee-4xl rounded-tl-4xl py-3 md:py-5 text-xs md:text-base">
+              Track Your Parcel
+            </button>
+
+            <button className="bg-black text-white p-2 md:p-3 text-lg md:text-2xl rounded-full">
+              <GoArrowUpRight />
+            </button>
+
+            <button className="btn badge-outline rounded-ee-4xl rounded-tl-4xl text-xs md:text-base">
+              Track Your Parcel
+            </button>
+          </div>
+        </div>
+
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
--- module-60/zap-shift-client/src/Pages/Home/Home/Home.jsx
+++ module-60/zap-shift-client/src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <h2>hello hi iam home</h2>
+            <Banner></Banner>
         </div>
     );
 };
