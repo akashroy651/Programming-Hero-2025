0c5c49ddb99a8df88361545ce716988e02a3df18
Akash Roy <akashroy65123@gmail.com>
2025-11-19 23:57:30 +0600

    aaded new commit file

--- module-60/zap-shift-client/src/Pages/Coverage/Coverage.jsx
+++ module-60/zap-shift-client/src/Pages/Coverage/Coverage.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css'
+import { useLoaderData } from "react-router";
+
 
 const Coverage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const position = [23.6850, 90.3563];
+
+  const serviceCenters = useLoaderData();
+  console.log(serviceCenters)
+
+  return (
+    <div>
+      <h2 className="text-5xl">We are available in 64 districts</h2>
+
+      <div></div>
+      {/*  */}
+      <div className="border w-full h-[800px]">
+        <MapContainer
+         center={position}
+          zoom={8}
+           scrollWheelZoom={false}
+           className="h-[800px]"
+           >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+        <Marker position={position}>
+                <Popup>
+                     A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+
+        </Marker>
+
+        </MapContainer>
+      </div>
+    </div>
+  );
 };
 
 export default Coverage;
--- module-60/zap-shift-client/src/routes/router.jsx
+++ module-60/zap-shift-client/src/routes/router.jsx
@@ -15,6 +15,7 @@ export const router = createBrowserRouter([
       {
         path: 'coverage',
         Component: Coverage,
+        loader: () => fetch ('/public/serviceCenters.json').then(res => res.json())
       }
     ]
   },
