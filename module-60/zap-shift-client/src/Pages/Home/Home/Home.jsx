c061b1bb54e356ba27a980e1ecf0b7a4243cb1ee
Akash Roy <akashroy65123@gmail.com>
2025-11-19 18:26:47 +0600

    added new file commit

--- module-60/zap-shift-client/src/Pages/Home/Home/Home.jsx
+++ module-60/zap-shift-client/src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Brands from '../Brands/Brands';
 
 const Home = () => {
     return (
         <div>
             <h2>hello hi iam home</h2>
             <Banner></Banner>
+            <Brands></Brands>
         </div>
     );
 };
