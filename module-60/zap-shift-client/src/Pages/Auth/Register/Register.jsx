7fe35063bdb5f9db24de779a73a21d0c7f3eacf0
Akash Roy <akashroy65123@gmail.com>
2025-11-22 13:47:10 +0600

    aaded file update photo

--- module-60/zap-shift-client/src/Pages/Auth/Register/Register.jsx
+++ module-60/zap-shift-client/src/Pages/Auth/Register/Register.jsx
@@ -1,18 +1,50 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import useAuth from "../../../hooks/useAuth";
+import { Link } from "react-router";
+import SocialLogin from "../SocialLogin/SocialLogin";
+import axios from "axios";
 
 const Register = () => {
 
     const {register, handleSubmit, formState: { errors }} = useForm();
 
-    const {registerUser} = useAuth();
+    const {registerUser, updateUserProfile} = useAuth();
 
     const handleRegistration = (data) => {
-        console.log('after register', data)
+        console.log('after register', data.photo[0]);
+        const profileImg = data.photo[0]
+
         registerUser(data.email, data.password)
         .then(result => {
             console.log(result.user)
+
+
+            // 1. store the image in form data
+            const formData = new FormData()
+            formData.append('image', profileImg)
+
+            // 2. send the photo url
+            const image_API_URL = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_host_key}`
+            
+            axios.post(image_API_URL, formData)
+            .then( res => {
+                console.log('afetr image upload',res.data.data.url)
+
+                     // update user profile to firebase
+                const  userProfile = {
+                    displayName : data.name,
+                    photoURL : res.data.data.url
+                }
+                updateUserProfile(userProfile)  
+                .then( () => {
+                    console.log('user profile updated done')
+                })
+                .catch(error => console.log(error))  
+
+            })
+
+       
         })
             .catch(error => {
                 console.log(error)
@@ -20,10 +52,28 @@ const Register = () => {
     }
 
   return (
-    <div>
-      <h2>hlw iam register now!</h2>
-      <form onSubmit={handleSubmit(handleRegistration)}>
+    <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+      <h3 className="text-3xl text-center">Welcome to zap shift</h3>
+        <p className="text-center">Please Register</p>
+      <form className="card-body" onSubmit={handleSubmit(handleRegistration)}>
         <fieldset className="fieldset">
+           
+            {/* name field */}
+          <label className="label">Name</label>
+          <input type="text" {...register('name', {required: true} )}
+          className="input" placeholder="Your name" />
+        { errors.name?.type === 'required' && <p 
+        className="text-red-500 font-semibold">name is required.</p>}
+            
+            {/* photo image field */}
+          <label className="label">Photo</label>
+          {/* <input type="file" className="file-input" /> */}
+          <input type="file" {...register('photo', {required: true} )}
+          className="file-input" placeholder="Your photo" />
+        { errors.name?.type === 'required' && <p 
+        className="text-red-500 font-semibold">photo is required.</p>}
+          
+            {/* email field */}
           <label className="label">Email</label>
           <input type="email" {...register('email', {required: true} )}
           className="input" placeholder="Email" />
@@ -58,7 +108,10 @@ const Register = () => {
           </div>
           <button className="btn btn-neutral mt-4">Register</button>
         </fieldset>
+        <p>Already have an account <Link 
+        className="text-blue-400 underline" to="/login">Login</Link></p>
       </form>
+      <SocialLogin></SocialLogin>
     </div>
   );
 };
--- module-60/zap-shift-client/src/contexts/AuthContext/AuthProvider.jsx
+++ module-60/zap-shift-client/src/contexts/AuthContext/AuthProvider.jsx
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
 
+const googleProvider = new GoogleAuthProvider();
+
+
 const AuthProvider = ({children}) => {
+        //  google login theke user details neyar jonno
+    const [user, setUser] = useState(null);
+    //  //
+    const [loading, setLoading] = useState(true);
 
     const registerUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const signInUser = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
+
+    const signInGoogle = () => {
+         setLoading(true)
+        return signInWithPopup(auth, googleProvider);
+    }
+
+
+    const logOut = () => {
+        setLoading(true)
+       return signOut(auth)
+    }
+
+
+    const  updateUserProfile = (profile) =>{
+        return updateProfile(auth.currentUser, profile)
+    }
+
+
+
+
+    //  observe user state 
+useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser)=> {
+        setUser(currentUser)
+        setLoading(false)
+    })
+    return () => {
+        unSubscribe();
+    }
+},[])
+
+
+
         const authInfo = {
+            user,
+            loading,
             registerUser,
             signInUser,
+            signInGoogle,
+            logOut,
+            updateUserProfile,
 
         }
 
