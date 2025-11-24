c5b68f83efc06425ceb8b5ac5d9402c63b0d40f8
Akash Roy <akashroy65123@gmail.com>
2025-11-25 01:47:25 +0600

    aded

---
+++ module-60/zap-shift-client/src/Pages/Dashboard/MyParcels/MyParcels.jsx
@@ -0,0 +1,96 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FiEdit } from "react-icons/fi";
+import { FaMagnifyingGlass, FaTrashCan } from "react-icons/fa6";
+import Swal from "sweetalert2";
+
+const MyParcels = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["myParcels", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels?email=${user.email}`);
+      return res.data;
+    },
+  });
+
+
+const handleParcelDelete = id => {
+    console.log(id)
+
+             Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+            if (result.isConfirmed) {
+                // Swal.fire({
+                // title: "Deleted!",
+                // text: "Your file has been deleted.",
+                // icon: "success"
+                // });
+            }
+            });
+
+}
+
+
+  return (
+    <div>
+      <h2>all of my parcels : {parcels.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Cost</th>
+              <th>payment Status</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+           
+           {
+            parcels.map((parcel, index) =>  <tr key={parcel._id}>
+              <th>{index + 1}</th>
+              <td>{parcel.parcelName}</td>
+              <td>{parcel.cost}</td>
+              <td>Blue</td>
+              <td>
+                <button className="btn btn-square hover:bg-primary">
+                    <FaMagnifyingGlass
+                     />
+                </button>
+                <button className="btn btn-square hover:bg-primary mx-2">
+                    <FiEdit />
+                </button>
+
+                <button
+                    onClick={ () => handleParcelDelete(parcel._id)}
+                 className="btn btn-square hover:bg-primary">
+                    <FaTrashCan />
+                </button>
+              </td>
+            </tr>
+            )
+           }
+          
+          
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default MyParcels;
